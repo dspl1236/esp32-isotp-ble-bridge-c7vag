830db1d31997d10006949e0ef195d51000ca72f0
dspl1236 <drewschlueter@gmail.com>
2026-03-15 16:20:07 -0400

    Add upstream source: persist.h

---
+++ main/persist.h
@@ -0,0 +1,20 @@
+#ifndef PERSIST_H
+#define PERSIST_H
+
+void 		persist_init();
+void 		persist_deinit();
+void		persist_start_task();
+void		persist_stop_task();
+
+void		persist_allow_send(uint16_t persist);
+uint16_t 	persist_enabled();
+void 		persist_set(uint16_t enable);
+int16_t 	persist_add(uint16_t rx, uint16_t tx, const void* src, size_t size);
+void 		persist_clear();
+void 		persist_task(void *arg);
+void 		persist_set_delay(uint16_t delay);
+void 		persist_set_q_delay(uint16_t delay);
+uint16_t 	persist_get_delay();
+uint16_t 	persist_get_q_delay();
+
+#endif
