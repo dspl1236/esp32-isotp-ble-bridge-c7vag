f2e972efb3efc5ddb86d22f4e0bd2885e1da7c75
dspl1236 <drewschlueter@gmail.com>
2026-03-19 22:22:52 -0400

    feat: declare bridge_received_wifi() in header

--- main/isotp_bridge.h
+++ main/isotp_bridge.h
@@ -9,6 +9,8 @@ void		isotp_stop_task();
 void		bridge_connect();
 void		bridge_disconnect();
 void		bridge_received_ble(const void* src, size_t size);
+void		bridge_received_wifi(uint16_t tx_id, uint16_t rx_id,
+							 const uint8_t *data, size_t len);
 int32_t		bridge_send_isotp(send_message_t *msg);
 uint16_t	bridge_send_available();
 
